45c7934d0091970d6e2df82c63622014171a2598
Mike Kershaw / Dragorn <dragorn@kismetwireless.net>
2017-10-04 15:07:01 -0400

    Import plugin data

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # IoD-Screwdriver
-Plugin for Kismet to detect and highlight IoD devices
+Plugin for Kismet to detect and highlight IoD devices.
+
+# Installation
+
+Clone this repository
+
+    `$ git clone https://github.com/internetofdongs/IoD-Screwdriver.git`
+
+Install the plugin.  Plugins can be installed system-wide (as root), or in your home directory.
+
+To install system-wide:
+
+    `$ cd plugin-iod-screwdriver`
+    `$ sudo make install`
+
+To install locally, as the user who will be running `kismet`:
+
+    `$ cd plugin-iod-screwdriver`
+    `$ make userinstall`
+
+And start Kismet!  If Kismet was already running, you will need to restart it.
+
+# Highlighting IoD devices
+
+Device highlighting can be configured via the Kismet web UI:
+
+1.  Go to the upper left sidebar ('hamburger') menu
+2.  Go to 'Settings'
+3.  Go to 'Device Row Highlighting'
+
---
+++ plugin-iod-screwdriver/httpd/js/iod_screwdriver.js
@@ -0,0 +1,56 @@
+(
+  typeof define === "function" ? function (m) { define("iod-screwdriver-js", m); } :
+  typeof exports === "object" ? function (m) { module.exports = m(); } :
+  function(m){ this.iod_screwdriver = m(); }
+)(function () {
+
+"use strict";
+
+var exports = {};
+
+// Flag we're still loading
+exports.load_complete = 0;
+
+kismet_ui.AddDeviceRowHighlight({
+    name: "IoD Device",
+    description: "Detect and highlight known IoD devices",
+    priority: -100,
+    defaultcolor: "#ff6699",
+    defaultenable: true,
+    fields: [
+        'kismet.device.base.phyname',
+        'kismet.device.base.name'
+    ],
+    selector: function(data) {
+        var bluetooth_names = [
+            '^Regex1.*',
+            '*Regex2*',
+            ];
+
+        var wifi_names = [
+            'some_wifi_regex',
+            ];
+
+        if (data['kismet.device.base.phyname'] === 'Bluetooth') {
+            for (var re of bluetooth_names) {
+                if (data['kismet.device.base.name'].match(re) != null)
+                    return true;
+            }
+        } else if (data['kismet.device.base.phyname'] === 'IEEE802.11') {
+            for (var re of wifi_names) {
+                if (data['kismet.device.base.name'].match(re) != null)
+                    return true;
+            }
+        }
+
+        return false;
+    }
+});
+
+
+// We're done loading
+exports.load_complete = 1;
+
+return exports;
+
+});
